f10e3276173681adae3cc0c045122a58bbb79ec0
Lauren Przywara <alicenoble.py@gmail.com>
2022-08-25 11:15:15 -0400

    Problem class from master

---
+++ src/function_1/Problem.java
@@ -0,0 +1,10 @@
+package function_1;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            System.out.println(j);
+        }
+    }//same code here master
+    //another code her master
+}
